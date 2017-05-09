b2614d273cddcea059edaf44e708336753032f28
Anabel Ramirez and John Dowd <anabel@>
2017-05-09 11:41:16 -0700

    add html, js for functioning movie ticket program.

---
+++ README.md
@@ -0,0 +1,47 @@
+# Movie Tickets
+
+#### An Epicodus exercise using objects and methods, 05.09.17
+
+#### **By Anabel Ramirez and John Dowd**
+
+## Description
+
+A movie ticket buying site.
+
+
+Specs:<br>
+
+|  behavior | input  | output  |
+|---|---|---|
+| select the name of a movie  | "Jaws"  | true  |
+| select time of the movie  | 2pm  | true  |
+| select type of ticket / age discount | senior | true |
+| number of tickets | 2 | 2 |
+| output ticket  | "Jaws:true, 2pm:true, senior:true" | "Jaws, 2pm, senior, $" |
+
+
+
+## Setup/Installation Requirements
+
+Clone this repository.
+Open the index page in a web browser.
+
+## Known Bugs
+
+No known bugs.
+
+## Support and contact details
+
+If you have any issues or have questions, ideas, concerns, or contributions please contact anabel@apparentinc.com.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Javascript
+* Bootstrap
+
+### License
+This software is licensed under the MIT license.
+
+Copyright (c) 2017 **Anabel Ramirez, John Dowd**
---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+//business logic
+function Movie (name, time, age, quantity) {
+  this.name = name;
+  this.time = time;
+  this.age = age;
+  this.quantity = quantity;
+}
+
+
+Movie.prototype.ticket = function() {
+  if (this.time <= "14:00") {
+    var price = 7;
+  } else if (this.time > "14:00" && this.age < 60) {
+    var price = 10;
+  } else if ( this.time > "14:00" && this.age >= 60) {
+    var price = 8;
+  }
+  return "$" + (this.quantity * price);
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("#add-tickets").click(function() {
+    $("#ticket-choice").append('<div id="ticket-choice">' +
+    '<h3>Select a ticket:</h3>' + '<select class="form-control" id="age">' + '<option value="8">Student/Senior Discount: $8</option>' + '<option value="7">Youth/Matinee: $7</option>' + '<option value="10">Regular Price: $10</option>' + '</select>' + '<h3>Select quantity:</h3>' + '<div class="form-group">' + '<label for="quantity">Quantity:</label>' + '<input type="number" class="form-control" id="quantity">' + '</div>');
+  });
+
+  $("form#movieChoices").submit(function(event) {
+    event.preventDefault();
+debugger;
+    var selectedName = $("#movieName").val();
+    var selectedTime = $("#movieTime").val();
+    var selectedAge = parseInt($("#age").val());
+    var selectedQty = parseInt($("input#quantity").val());
+    var newTicket = new Movie(selectedName, selectedTime, selectedAge, selectedQty);
+    var total = newTicket.ticket();
+
+    $("#show-ticket").show();
+    $(".movie-name").append(selectedName);
+    $(".movie-time").append(selectedTime);
+    $(".quantity").append(selectedQty);
+    $(".total").append(total);
+  });
+});
